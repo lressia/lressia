b57e1748d0dc01bbcf14af86256d6cb63c7084a7
Lorenzo Ressia <103857731+lressia@users.noreply.github.com>
2022-05-27 16:42:08 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,16 @@
-hello
-ni hao
+#BLACKJACK 2.0
+
+import random
+
+card = 2, 3, 4, 5, 6, 7, 8, 9, 10, 'AS', 'J', 'Q', 'K'
+pale = '♠', '♥', '♦', '♣'
+def game():
+    print('empiece a jugar')
+
+def card_generator(card, pale):
+
+    random_num_fig_player = random.choice(card)
+    random_pale_player = random.choice(pale)
+    random_num_fig_crupier = random.choice(card)
+    random_pale_crupier = random.choice(pale)
+    print(random_num_fig_player, random_pale_player)
